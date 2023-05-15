55b5ebfa9c188a7b77eaa239fd2308d86d84b546
chuy-zip <andreschuy03@gmail.com>
2023-05-15 11:58:56 -0600

    Implemented vertex class

--- src/Vertex.java
+++ src/Vertex.java
@@ -4,5 +4,32 @@ public class Vertex {
     private String City;
     private ArrayList<Connection> neighbors;
 
+    public Vertex(String city) {
+        this.City = city;
+        this.neighbors = new ArrayList<>();
+    }
 
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
+
+    public ArrayList<Connection> getNeighbors() {
+        return neighbors;
+    }
+
+    public void setNeighbors(ArrayList<Connection> neighbors) {
+        this.neighbors = neighbors;
+    }
+
+    public void addNeighbor(Connection connection) {
+        neighbors.add(connection);
+    }
+
+    public void removeNeighbor(Connection connection) {
+        neighbors.remove(connection);
+    }
 }
